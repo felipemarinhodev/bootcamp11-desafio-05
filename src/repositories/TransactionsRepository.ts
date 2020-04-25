7f30190e3803ea5ed50faa31b5bbac9bf5422486
Felipe Marinho <felipmarinho@gmail.com>
2020-04-25 20:43:50 -0300

    implementing the challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,17 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface ListTransactionDTO {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +24,33 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): ListTransactionDTO {
+    const balance = this.getBalance();
+    return { transactions: this.transactions, balance };
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.sumValueByType('income');
+    const outcome = this.sumValueByType('outcome');
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create(data: CreateTransactionDTO): Transaction {
+    const { title, value, type } = data;
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
+  }
+
+  private sumValueByType(type: 'income' | 'outcome'): number {
+    const sum = this.transactions.reduce((total, transaction) => {
+      if (transaction.type === type) {
+        return total + transaction.value;
+      }
+      return total;
+    }, 0);
+    return sum;
   }
 }
 
